32156a87bd47fda7c09a341f9609d6314f6ee539
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-03-09 10:20:56 -0800

    using structural directives to loop through recipes and ingredients, adding hardcoded dummy recipes

--- recipe-book/src/app/shopping-list/shopping-list.component.ts
+++ recipe-book/src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ShoppingListAddComponent } from "./shopping-list-add.component";
+import { Ingredient } from '../recipes/ingredient';
+
 @Component({
   selector: 'rb-shopping-list',
   templateUrl: './shopping-list.component.html'
 })
 export class ShoppingListComponent implements OnInit {
+  items: Ingredient[] = [];
 
   constructor() { }
 
