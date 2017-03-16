81cf27d361b398d1a099095932ba2d9e1f59b352
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-03-16 14:48:42 -0700

    refactoring to include services

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe';
 import { RecipeItemComponent } from './recipe-item.component';
+import { RecipeService } from "../recipe.service";
 
 @Component({
   selector: 'rb-recipe-list',
@@ -9,14 +10,14 @@ import { RecipeItemComponent } from './recipe-item.component';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [
-  new Recipe('Pasta Alla Amatriciana', 'Delicioso', 'http://www.seriouseats.com/recipes/assets_c/2011/02/20110217-127355-dinner-tonight-pasta-all-amatriciana-thumb-625xauto-141251.jpg', []),
-  new Recipe('Detroit Style Pizza', 'Ooey Gooey', 'http://www.seriouseats.com/recipes/assets_c/2017/02/20170216-detroit-style-pizza-43-thumb-1500xauto-436479.jpg', []),
-  ];
+  recipes: Recipe[] = [];
+
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  constructor() { }
+
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onSelected(recipe: Recipe){
--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Recipe } from "./recipe";
 
 @Injectable()
 export class RecipeService {
-
+    private recipes: Recipe[] = [
+        new Recipe('Pasta Alla Amatriciana', 'Delicioso', 'http://www.seriouseats.com/recipes/assets_c/2011/02/20110217-127355-dinner-tonight-pasta-all-amatriciana-thumb-625xauto-141251.jpg', []),
+        new Recipe('Detroit Style Pizza', 'Ooey Gooey', 'http://www.seriouseats.com/recipes/assets_c/2017/02/20170216-detroit-style-pizza-43-thumb-1500xauto-436479.jpg', []),
+    ];
   constructor() { }
+
+  getRecipes(){
+    return this.recipes;
+  }
 
 }
